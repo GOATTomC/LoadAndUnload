63762d14d6e2fbdbf26bd18d64c2b3812979cafc
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2018-11-26 12:14:22 +0100

    Shelf and ProductInfo component

---
+++ Load&Unload/Assets/Scripts/ProductShelf/Shelf.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Shelf : MonoBehaviour {
+
+    public event Action OnFull;
+
+    [SerializeField]
+    private int m_ProductAmount;
+
+    [SerializeField]
+    private string m_ProductName;
+
+    private List<ProductInfo> m_Products;
+
+    // Called when product is dropped into shelf
+    private void AddProduct(ProductInfo product)
+    {
+        //Check if we want this product in this shelf
+        if (product.ProductName == m_ProductName)
+        {
+            m_Products.Add(product);
+
+            if (m_Products.Count == m_ProductAmount)
+            {
+                if (OnFull != null)
+                {
+                    OnFull.Invoke();
+                }
+            }
+        }
+    }
+
+    private void RemoveProduct(ProductInfo product)
+    {
+        if (m_Products.Contains(product))
+        {
+            m_Products.Remove(product);
+        }
+    }
+}
